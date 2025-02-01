1d0c7b599a6d33ddafbe84147ba7c7fd4423775d
Rint-Aley <rint.aley@gmail.com>
2025-02-01 17:11:13 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Huffman coding" << std::endl;
+    return 0;
+}
---
+++ tests/test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(GtestWorksProperly, OneEqualOne) {
+	ASSERT_EQ(1, 1);
+}
